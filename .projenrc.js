8e87134055e4c4362d8fe392b6d0a8df48167910
Robert Koch <robert@kochie.io>
2021-09-20 11:53:15 +1000

    updated projen and cdk

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,8 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, ProjectType } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Robert Koch',
   authorAddress: 'robert@kochie.io',
-  cdkVersion: '2.0.0-rc.5',
+  cdkVersion: '2.0.0-rc.21',
   defaultReleaseBranch: 'main',
   name: 'aws-lambda-golang-cdk-v2',
   repositoryUrl: 'https://github.com/kochie/aws-lambda-golang-cdk-v2.git',
@@ -10,20 +10,17 @@ const project = new AwsCdkConstructLibrary({
   // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
   deps: [
-    'constructs@10.0.5',
-    'aws-cdk-lib@2.0.0-rc.5',
+    'aws-cdk-lib@2.0.0-rc.21',
+    'constructs@^10.0.5',
   ] /* Runtime dependencies of this module. */,
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  description:
+    'CDK construct for golang files' /* The description is just a string that helps people understand the purpose of the package. */,
   devDeps: [
-    'constructs@10.0.5',
-    'aws-cdk-lib@2.0.0-rc.5',
+    'aws-cdk-lib@2.0.0-rc.21',
   ] /* Build dependencies for this module. */,
-  peerDeps: [
-    'constructs@10.0.5',
-    'aws-cdk-lib@2.0.0-rc.5',
-  ],
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  peerDeps: ['aws-cdk-lib@2.0.0-rc.21'],
+  packageName: 'aws-lambda-golang-cdk-v2' /* The "name" in package.json. */,
+  // projectType: ProjectType  /* Which type of project this is (library/app). */
   // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
 });
 project.synth();
